c752aa873963963852b7b8876cf1541ba67c5602
pushnyakovsa <pushnyakovsa@gmail.com>
2016-09-17 01:15:54 +0300

    Created by Coding Ground Client

---
+++ hh-school-2016/task_das.cs
@@ -0,0 +1,48 @@
+// Дано равенство, в котором цифры заменены на буквы:
+// rqtr + rqrs = squt 
+//
+// Найдите сколько у него решений, если различным буквам соответствуют различные цифры (ведущих нулей в числе не бывает).
+
+using System;
+
+namespace task_das
+{
+    public class das
+    {
+        public long get()
+        {
+            long output_param = 0;
+            
+            int r = 0;
+            int s = 0;
+            int t = 0;
+            int u = 0;
+            int q = 0;
+            
+            for (r = 1; r <= 4; r++)
+            {
+                for (s = 2; s <= 9; s++)
+                {
+                    for (t = 0; t <= 9; t++)
+                    {
+                        if (t != 1 || t != 2)
+                        {
+                            for (u = 1; u <= 9; u++)
+                            {
+                                for (q = 0; q <= 9; q++)
+                                {
+                                    if ((1000 * r + 100 * q + 10 * t + r) + (1000 * r + 100 * q + 10 * r + s) == (1000 * s + 100 * q + 10 * u + t))
+                                    {
+                                        output_param = output_param + 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        
+            return output_param;
+        }
+    }
+}
