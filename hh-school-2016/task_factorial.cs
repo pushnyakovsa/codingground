ec2532611d5602c815fc390e0bc76c32e4effdd0
pushnyakovsa <pushnyakovsa@gmail.com>
2016-09-20 11:30:06 +0300

    Created by Coding Ground Client

---
+++ hh-school-2016/task_factorial.cs
@@ -0,0 +1,63 @@
+// Наименьшее число m, такое, что m! делится без остатка на 10 — это m=5 (5! = 120).
+// Аналогично, наименьшее число m, такое, что m! делится без остатка на 25 — это m=10.
+// В общем случае, значение функции s(n) равно наименьшему числу m, такому что m! без остатка делится на n.
+// Определим функцию S(M, N) = ∑s(n) для всех n ∈ [M, N]. К примеру, S(6, 10) = 3 + 7 + 4 + 6 + 5 = 25.
+// Найдите S(670000000, 680000000).
+
+using System;
+
+namespace task_factorial
+{
+    public class factorial
+    {
+        public static long fact(long a, long b)
+        {
+            long output_param = 1;
+            
+            if (b != 0)
+            {
+                output_param = b * a;
+            }
+            else if (b == 0)
+            {
+                if (a == 1)
+                {
+                    return output_param;
+                }
+                else if (a > 1)
+                {
+                    for (int i = 2; i <= a; i++)
+                    {
+                        output_param = output_param * i;
+                    }
+                }
+            }
+            
+            return output_param;
+        }
+        
+        public long get(long m, long n, long x, long y)
+        {
+            int exit_cond = 1;
+
+            do
+            {
+                for (long i = m; i <= n; i++)
+                {
+                    if ((fact(y + 1, x) % i) == 0)
+                    {
+                        return y + 1;
+                    }
+                    else
+                    {
+                        x = fact(y + 1, x);
+                        y++;
+                        exit_cond++;
+                    }
+                }
+            } while (exit_cond <= 10);
+            
+            return 0;
+        }
+    }
+}
